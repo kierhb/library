ea5e85c3fca844c293425c9cf59912eca19e50dc
kierhb <kierbobila@gmail.com>
2023-02-01 14:28:46 +0800

    Rebuild library then finish style for header

--- library/main.js
+++ library/main.js
@@ -1,97 +1,23 @@
-const popup = document.querySelector(".popup")
-const addBtn = document.querySelector("#add-btn")
-const closeBtn = document.querySelector("#close-btn")
-const submitBtn = document.querySelector("#submit--btn")
-const delAllBtn = document.querySelector("#del-all-btn")
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
 
-const ulEl = document.querySelector(".ul-el");
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-delAllBtn.addEventListener("click", function() {
-    localStorage.clear()
-    myLibrary = []
-    ulEl.innerHTML = ""
-    render()
-})
-
-addBtn.addEventListener("click", function() {
-    popup.classList.add("active")
-})
-
-closeBtn.addEventListener("click", function() {
-    popup.classList.remove("active")
-})
-
-/*
-submitBtn.addEventListener("click", function() {
-    popup.classList.remove("active")
-}) 
-*/
-
-const booksFromLocalStorage = JSON.parse(localStorage.getItem("myBooks"))
-
-let myLibrary = !booksFromLocalStorage ? [] : JSON.parse(localStorage.getItem("myBooks"))
-
-const bookTitle = document.querySelector("#book--title")
-const bookAuthor = document.querySelector("#book--author")
-const bookPages = document.querySelector("#book--pages")
-const bookRead = document.querySelector("#book--checkbox")
-
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-}
-
-//* Retrieve book info
-function getBookInfo() {
-    const title = bookTitle.value
-    const author = bookAuthor.value
-    const pages = bookPages.value
-    const isRead = bookRead.checked
-    return new Book(title, author, pages, isRead)
-}
-
-function addBookToLibrary() {
-    getBookInfo()
-    myLibrary.push(getBookInfo())
-    localStorage.setItem("myBooks", JSON.stringify(myLibrary))
-}
-
-submitBtn.addEventListener("click", function(event) {
-    addBookToLibrary()
-    render()
-    popup.classList.remove("active")
-    event.preventDefault()
-    
-    bookTitle.value = ""
-    bookAuthor.value = ""
-    bookPages.value = ""
-    bookRead.checked = false
-})
-
-
-function render() {
-    const myBooks = JSON.parse(localStorage.getItem("myBooks"))
-    const readBtn = document.querySelectorAll("#read-btn")
-    const removeBtn = document.querySelectorAll("#remove-btn")
-
-    let bookItems = ""
-
-    myBooks.forEach((book) => {
-        bookItems += `
-            <li class="book-el">
-                <h3 id="title">${book.title}</h3>
-                <h4 id="author">by ${book.author}</h4>
-                <div id="pages">${book.pages} pages</div>
-                ${book.read ? 
-                    `<button id="unread-btn">Unread</button>` : 
-                    `<button id="read-btn">Read</button>`}
-                <button onclick="remove()" id="remove-btn">Remove</button>
-            </li>
-        `
-        ulEl.innerHTML = bookItems
-    });
-}
-
-render()
+setupCounter(document.querySelector('#counter'))
