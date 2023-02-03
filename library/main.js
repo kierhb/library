6b7cb349155d39b59b85d8e74019cd9cd66a8668
kierhb <kierbobila@gmail.com>
2023-02-03 11:06:55 +0800

    Finish popup form animation

--- library/main.js
+++ library/main.js
@@ -1,23 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const addBook = document.querySelector("#add---book")
+const closeBtn = document.querySelector(".close-btn")
+const submitBtn = document.querySelector("#submit-btn")
+const popup = document.querySelector(".popup")
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+addBook.addEventListener("click", function() {
+    popup.classList.add("active")
+})
+
+closeBtn.addEventListener("click", function() {
+    popup.classList.remove("active")
+})
+
+submitBtn.addEventListener("click", function() {
+    popup.classList.remove("active")
+})
