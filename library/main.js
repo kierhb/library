c928f9aacd35a6ac09794ab2b56be61385420906
kierhb <kierbobila@gmail.com>
2023-02-03 14:53:58 +0800

    Create function to add book to library

--- library/main.js
+++ library/main.js
@@ -12,6 +12,43 @@ closeBtn.addEventListener("click", function() {
     popup.classList.remove("active")
 })
 
+
+
+//? Retrieve book information and save to localStorage
+
+let myLibrary = []
+
+const bookCategory = document.querySelector("#category")
+const bookTitle = document.querySelector("#title")
+const bookAuthor = document.querySelector("#author")
+const bookPages = document.querySelector("#pages")
+const bookIsRead = document.querySelector("#checkbox")
+
+function Book(category, title, author, pages, isRead) {
+    this.category = category
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+}
+
+function getBookInfo() {
+    const bkCategory = bookCategory.value
+    const bkTitle = bookTitle.value
+    const bkAuthor = bookAuthor.value
+    const bkPages = bookPages.value
+    const bkIsRead = bookIsRead.checked
+    return new Book(bkCategory, bkTitle, bkAuthor, bkPages, bkIsRead)
+}
+
+function addBookToLibrary() {
+    getBookInfo()
+    myLibrary.push(getBookInfo())
+}
+
 submitBtn.addEventListener("click", function() {
     popup.classList.remove("active")
+
+    addBookToLibrary()
+    console.log(myLibrary)
 })
